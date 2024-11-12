e09a22cb652f7c9c6191e8cabf41eb13f48caf52
David Cardoso <david.cardoso04@live.com>
2024-11-12 10:11:18 +0000

    v1.0
    Main

---
+++ src/main/java/com/example/champeons/ChampeonsApplication.java
@@ -0,0 +1,13 @@
+package com.example.champeons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampeonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChampeonsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/champeons/ChampeonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.champeons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampeonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
